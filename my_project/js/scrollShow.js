e08222047eeee5e0a064068ae30b03eb968e8119
KimGyeongHyun <potter9602@naver.com>
2023-08-20 14:59:39 +0900

    study react, update my project
    add comment to react /
    add slide menu  in menu section /
    add info-2 section

--- my_project/js/scrollShow.js
+++ my_project/js/scrollShow.js
@@ -1,6 +1,6 @@
 setInterval(function () {
   var nav = $(".tracking-nav");
   var h = $(window).scrollTop();
-  if (h > 500) nav.animate({ left: "1vw" }, 200);
+  if (h > 50) nav.animate({ left: "1vw" }, 200);
   else nav.animate({ left: "-10vw" }, 200);
 }, 400);
