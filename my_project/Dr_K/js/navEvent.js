c00967dcc53e22456c10b420260e3a6cadedc120
KimGyeongHyun <potter9602@naver.com>
2023-09-04 17:38:43 +0900

    update my project
    front

--- my_project/Dr_K/js/navEvent.js
+++ my_project/Dr_K/js/navEvent.js
@@ -1,8 +1,10 @@
 var nav = document.querySelector(".nav");
 var white = document.querySelector(".bg-white");
 var logo = document.querySelector(".nav .nav-menu .logo");
-var li = document.getElementsByClassName("nav-list");
-var icon = document.getElementsByClassName("icon-toggle");
+var li = document.getElementsByClassName("tog-li");
+var spacer = document.getElementsByClassName("tog-sp");
+var icon = document.getElementsByClassName("tog-ic");
+var popul = document.getElementsByClassName("tog-pop");
 var [flag1, flag2] = [false, false];
 
 function changeBlack() {
@@ -15,6 +17,10 @@ function changeBlack() {
   for (var i = 0; i < icon.length; i++) {
     icon[i].style.color = "black";
     icon[i].style.textShadow = "none";
+  }
+  for (var i = 0; i < spacer.length; i++) {
+    spacer[i].style.backgroundColor = "black";
+    spacer[i].style.boxShadow = "none";
   }
 }
 
@@ -29,6 +35,18 @@ function changeWhite() {
     icon[i].style.color = "white";
     icon[i].style.textShadow = "2px 2px 4px #333333";
   }
+  for (var i = 0; i < spacer.length; i++) {
+    spacer[i].style.backgroundColor = "white";
+    spacer[i].style.boxShadow = "1px 1px 1px 0px #666666";
+  }
+}
+
+function hoverMenu() {
+  popul[0].style.height = "160px";
+}
+
+function outMenu() {
+  popul[0].style.height = "0px";
 }
 
 function changeWhenEvent() {
@@ -42,10 +60,12 @@ function changeWhenEvent() {
 nav.addEventListener("mouseover", () => {
   flag2 = true;
   changeWhenEvent();
+  hoverMenu();
 });
 nav.addEventListener("mouseout", () => {
   flag2 = false;
   changeWhenEvent();
+  outMenu();
 });
 
 window.addEventListener("scroll", (event) => {
