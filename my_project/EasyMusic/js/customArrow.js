b7f49720021592f174f7d0320132af16cc805bcf
KimGyeongHyun <potter9602@naver.com>
2023-09-08 17:16:12 +0900

    update easy music
    add footer /
    add scroll event /
    add horizontal slide navigation /
    add detail

---
+++ my_project/EasyMusic/js/customArrow.js
@@ -0,0 +1,6 @@
+document.querySelector(".fp-prev").innerHTML =
+  '<i class="fa-solid fa-chevron-left fa-xl"></i>';
+document.querySelector(".fp-prev").style.opacity = 0;
+
+document.querySelector(".fp-next").innerHTML =
+  '<i class="fa-solid fa-chevron-right fa-xl"></i>';
---
+++ my_project/EasyMusic/js/scrollEvent.js
@@ -0,0 +1,3 @@
+$("#top").click(() => {
+  fullpage_api.moveTo(1);
+});
