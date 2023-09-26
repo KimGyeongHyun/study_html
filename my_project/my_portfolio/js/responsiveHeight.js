186e561b43e80796be86ca6e3f667088313835e9
KimGyeongHyun <105264179+KimGyeongHyun@users.noreply.github.com>
2023-09-26 22:56:55 +0900

    test commit

--- my_project/my_portfolio/js/responsiveHeight.js
+++ my_project/my_portfolio/js/responsiveHeight.js
@@ -1,12 +1,16 @@
-// JavaScript to adjust the height on page load and on window resize
-function setElementHeight() {
-  const element = document.querySelector("section");
-  const windowHeight = window.innerHeight;
+// // JavaScript to adjust the height on page load and on window resize
+// function setElementHeight() {
+//   const element = document.querySelector("section");
+//   const windowHeight = window.innerHeight;
 
-  // Adjust the element's height to the viewport height
-  element.style.height = windowHeight + "px";
-}
+//   // Adjust the element's height to the viewport height
+//   element.style.height = windowHeight + "px";
+// }
 
-// Call the function on page load and when the window is resized
-window.addEventListener("load", setElementHeight);
-window.addEventListener("resize", setElementHeight);
+// // Call the function on page load and when the window is resized
+// window.addEventListener("load", setElementHeight);
+// window.addEventListener("resize", setElementHeight);
+
+window.onload = function () {
+  window.scrollTo(0, 1); // Scroll down by 1 pixel to trigger auto-hide
+};
