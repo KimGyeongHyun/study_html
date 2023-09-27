495162933a960c86e414ad5f1297f764f91ce60f
KimGyeongHyun <potter9602@naver.com>
2023-09-27 16:37:22 +0900

    update my portfolio
    add AOS in header logo

---
+++ my_project/my_portfolio/js/headerAOS.js
@@ -0,0 +1,20 @@
+document
+  .querySelector(".header .text-box .head1")
+  .getAnimations()
+  .forEach((animation) => {
+    animation.effect.composite = "add";
+  });
+
+document
+  .querySelector(".header .text-box .head2")
+  .getAnimations()
+  .forEach((animation) => {
+    animation.effect.composite = "add";
+  });
+
+document
+  .querySelector(".header .text-box .head3")
+  .getAnimations()
+  .forEach((animation) => {
+    animation.effect.composite = "add";
+  });
